23b79a3417f99a129250679057c4b80a8d0eb323
unknown <codewithanas12@gmail.com>
2025-09-14 16:26:25 +0500

    .

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ st.set_page_config(
 
 # ----------------- THEME HANDLING -----------------
 if 'theme' not in st.session_state:
-    st.session_state.theme = "dark"
+    st.session_state.theme = "light"
 
 def toggle_theme():
     st.session_state.theme = "light" if st.session_state.theme == "dark" else "dark"
